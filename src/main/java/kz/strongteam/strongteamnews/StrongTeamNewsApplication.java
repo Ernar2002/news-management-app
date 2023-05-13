4d002a7216167945e239b36f26bfbbbe9456b70f
Ernar2002 <erjan.ernar2002@gmail.com>
2023-05-13 22:03:12 +0300

    feature/integrated swagger

--- src/main/java/kz/strongteam/strongteamnews/StrongTeamNewsApplication.java
+++ src/main/java/kz/strongteam/strongteamnews/StrongTeamNewsApplication.java
@@ -2,7 +2,10 @@ package kz.strongteam.strongteamnews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -10,6 +13,20 @@ public class StrongTeamNewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StrongTeamNewsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
--- src/main/java/kz/strongteam/strongteamnews/controllers/NewsController.java
+++ src/main/java/kz/strongteam/strongteamnews/controllers/NewsController.java
@@ -1,11 +1,14 @@
 package kz.strongteam.strongteamnews.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import kz.strongteam.strongteamnews.dto.request.NewsDtoRequest;
 import kz.strongteam.strongteamnews.dto.response.NewsDtoResponse;
 import kz.strongteam.strongteamnews.services.interfaces.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +16,13 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/news/")
+@RequestMapping(value = "/api/v1/news/", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class NewsController {
 
     private final NewsService newsService;
 
+    @Operation(summary = "Get all news")
     @GetMapping("")
     public ResponseEntity<List<NewsDtoResponse>> getAll(
             @RequestParam(defaultValue = "0") int page,
@@ -28,11 +32,13 @@ public class NewsController {
         return ResponseEntity.ok(newsService.getAll(pageable));
     }
 
+    @Operation(summary = "Get news by id")
     @GetMapping("{id}")
     public ResponseEntity<NewsDtoResponse> getById(@PathVariable UUID id) {
         return ResponseEntity.ok(newsService.getById(id));
     }
 
+    @Operation(summary = "Get all news by source id")
     @GetMapping("source/{sourceId}")
     public ResponseEntity<List<NewsDtoResponse>> getBySourceId(
             @PathVariable UUID sourceId,
@@ -43,6 +49,7 @@ public class NewsController {
         return ResponseEntity.ok(newsService.getNewsBySourceId(sourceId, pageable));
     }
 
+    @Operation(summary = "Get all news by topic id")
     @GetMapping("topic/{topicId}")
     public ResponseEntity<List<NewsDtoResponse>> getByTopicId(
             @PathVariable UUID topicId,
@@ -53,16 +60,19 @@ public class NewsController {
         return ResponseEntity.ok(newsService.getNewsByTopicId(topicId, pageable));
     }
 
+    @Operation(summary = "Create new news")
     @PostMapping("")
     public ResponseEntity<NewsDtoResponse> create(@RequestBody NewsDtoRequest newsDtoRequest) {
         return ResponseEntity.ok(newsService.save(newsDtoRequest));
     }
 
+    @Operation(summary = "Update news")
     @PutMapping("{id}")
     public ResponseEntity<NewsDtoResponse> update(@PathVariable UUID id, @RequestBody NewsDtoRequest newsDtoRequest) {
         return ResponseEntity.ok(newsService.update(id, newsDtoRequest));
     }
 
+    @Operation(summary = "Delete news")
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         newsService.delete(id);
